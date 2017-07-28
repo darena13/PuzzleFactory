b3d779e7cfac7dc23e0dcc76a94ce93193714787
Daria K <darena13@mail.ru>
2017-07-28 12:47:43 +0300

    There is nothing.

--- app/src/main/java/darena13/puzzlefactory/MainActivity.java
+++ app/src/main/java/darena13/puzzlefactory/MainActivity.java
@@ -1,13 +1,53 @@
 package darena13.puzzlefactory;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    LinearLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        final DrawView drawView = new DrawView(this);
+        setContentView(drawView);
+
+//        drawView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawView.changeColor();
+//            }
+//        });
+
+        button = new Button(getApplicationContext());
+        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    }
+
+    class DrawView extends View {
+
+        public DrawView(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw (Canvas canvas) {
+            canvas.drawColor(Color.GREEN);
+            addContentView(button, layoutParams);
+        }
+
+        void changeColor (Canvas canvas) {
+            canvas.drawColor(Color.RED);
+        }
     }
 }
